755f9ceaa49ad1c41a17b7e0a2ed9e1c6d07ca4c
Anna <annarainchik@gmail.com>
2022-06-13 18:51:32 +0300

    change gradle

--- src/test/java/owner/tests/ApiTest.java
+++ src/test/java/owner/tests/ApiTest.java
@@ -6,8 +6,7 @@ import owner.config.ApiConfig;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.hamcrest.Matchers.is;
+
 
 public class ApiTest {
 
@@ -16,14 +15,12 @@ public class ApiTest {
 
         ApiConfig config = ConfigFactory.create(ApiConfig.class, System.getProperties());
         given()
-                .log().body()
+
                 .contentType(JSON)
                 .when()
                 .queryParam(config.token())
                 .get(config.baseUrl())
                 .then()
-                .log().status()
-                .log().body()
                 .statusCode(200);
 
     }
